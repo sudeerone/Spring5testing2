8bd7fe1fe7bdc963f66c04b4db7f6d628db78d06
testgit <testgit@testgiting.com>
2017-12-05 17:52:45 -0800

    Initial Commit Test

---
+++ src/main/java/guru/springframework/test/springframework5test/Springframework5testApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.test.springframework5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springframework5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springframework5testApplication.class, args);
+	}
+}
